1a18aacdeaa096fa168a22ca34e476f44d464edb
BrunoSaade <brsaade@gamil.com>
2023-10-17 14:35:06 -0300

    Feat: Added section to add contact

--- partials/contact/ContactList.vue
+++ partials/contact/ContactList.vue
@@ -1,6 +1,42 @@
 <template lang="pug">
 .contact-list
-  p.text-white.place-self-start.mb-0.text-s6 Friends
+  .contact-list--header
+    p.text-white.place-self-start.mb-0.text-s6 Friends
+    VButton(
+      v-if="!mustShowAddFriendSection"
+      @click="mustShowAddFriendSection = true"
+      type="submit" 
+      variant="success" 
+      size="small"
+      class="!w-[70px]"
+    ) Add
+  .contact-list--add-contact(v-if="mustShowAddFriendSection")
+    VForm#add.w-full(
+      @submit="handleAddFriend" 
+      name="addFriend",
+    )
+      template
+        fieldset.home--inputs
+          label.text-white.mb-1 Add friend
+          VTextInput#friendEmail(
+            variant='primary'
+            v-model='friendEmail' 
+            placeholder="Type friend's email"
+            rules="required|email"
+          )
+          .l-inline-center-center-nowrap.gap-2.my-4
+            VButton(
+              @click="mustShowAddFriendSection = false"
+              size="small"
+              type="button"
+              variant="outline_tertiary"
+            ) Cancel
+            VButton(
+              size="small"
+              type="submit" 
+              variant="tertiary"
+            ) Add
+          hr
   ContactCard(
     v-ripple
     v-for="(chat, index) in chats" 
@@ -23,7 +59,9 @@ export default {
         {
           name: "Rafael Saade"
         },
-      ]
+      ],
+      friendEmail: '',
+      mustShowAddFriendSection: false,
     }
   },
   mounted() {},
@@ -31,6 +69,9 @@ export default {
   methods: {
     handleOpenChat(chat) {
       console.log(chat)
+    },
+    handleAddFriend() {
+      console.log("Add friend")
     }
   },
 }
@@ -39,5 +80,11 @@ export default {
 <style lang="postcss">
 .contact-list {
   @apply l-stack-center-center w-full gap-3;
+}
+.contact-list--header {
+  @apply l-inline-between-center w-full gap-3;
+}
+.contact-list--add-contact {
+  @apply l-inline-start-center w-full;
 }
 </style>
