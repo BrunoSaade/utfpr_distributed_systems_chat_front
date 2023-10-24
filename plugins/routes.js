cf297abe633bcc430c2bb572710701c1da9b25ed
BrunoSaade <brsaade@gmail.com>
2023-10-24 17:52:53 -0300

    Feat: Vue route added to handle it

---
+++ plugins/routes.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+export default ({ app, store }) => {
+  app.router.beforeEach((to, from, next) => {
+    const isAuthenticated = !!window.localStorage.getItem('token')
+    const isPrivateRoute = to.path.startsWith('/main')
+    const { path, hash } = to
+    if (!path.endsWith('/')) {
+      return next({ path: path + '/' })
+    }
+
+    if (isPrivateRoute && !isAuthenticated) {
+      return next({ path: '/' })
+    }
+
+    if (!isPrivateRoute && isAuthenticated) {
+      return next({ path: '/main/chat/' })
+    }
+  
+    const { mainContent } = window
+    if (mainContent?.scrollTo && !hash) mainContent?.scrollTo(0, 0)
+    if (window.scrollTo && !hash) window.scrollTo(0, 0)
+    next()
+  })
+}
