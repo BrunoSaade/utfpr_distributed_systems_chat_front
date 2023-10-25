42e2d8d1d2d8485f64d1ad4dbb386583e3289e76
BrunoSaade <brsaade@gmail.com>
2023-10-25 11:02:32 -0300

    Feat: ChatMessage and Chat partials created

---
+++ partials/chat/Chat.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.chat(class="bg-primary-800 relative flex flex-col")
+  ChatHeader(:chat="selectedChat")
+  ChatBody
+  ChatInputMessage
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex"
+export default {
+  name: "Chat",
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState({
+      selectedChat: (state) => state.selectedChat
+    }),
+  },
+  created() {},
+  methods: {},
+}
+</script>
+
+<style lang="postcss" scoped>
+.chat {
+  @apply flex h-screen;
+}
+</style>
+  
---
+++ partials/chat/ChatMessage.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+.chat-message(:class="isFriendMessage ? 'text-left' : 'text-right'")
+  .chat-message-content(:class="isFriendMessage ? 'friend-message': 'user-message'")
+    p.text-s3.mb-0 {{ message }}
+</template>
+
+<script>
+export default {
+  name: "ChatMessage",
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    isFriendMessage: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  created() {},
+  methods: {},
+}
+</script>
+
+<style lang="postcss" scoped>
+.chat-message {
+  @apply my-2;
+}
+.chat-message-content {
+  padding: 10px;
+  display: inline-block;
+  max-width: 80%;
+  margin: 5px;
+  max-height: 100%;
+  word-wrap: break-word;
+}
+
+.friend-message {
+  background-color: #DCF8C6; /* Cor de fundo das mensagens do usuário */
+  border-radius: 15px 15px 15px 0;
+}
+
+.user-message {
+  background-color: #E5E5EA; /* Cor de fundo das mensagens do amigo (exemplo) */
+  text-align: left;
+  border-radius: 15px 15px 0 15px;
+}
+</style>
